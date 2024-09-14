763c0312e11e68c799ca4e0f2070cb9206600240
islam2718 <msialhn@gmail.com>
2024-09-14 23:50:27 +0600

    learning c# created and 1 to 7 done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Test{
+    public static void Main(string[] args){
+        // Console.WriteLine("My Name is: Islam Hossain");
+        /*
+        Console.WriteLine("I am 33 Years old");
+        Console.WriteLine("I am trying to be a Full-Stack Developer :)");
+        */
+        
+        Console.Write("My Name is: Islam Hossain\n");
+        Console.WriteLine("\tI am \"33\" Years old");
+        Console.WriteLine("I am trying to be a Full-Stack Developer :)");
+    }
+}
