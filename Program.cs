2c0d262a3f86758686851a29ab11503f21d65f9f
Islam2718 <mislamhn@gmail.com>
2024-09-17 17:27:26 +0600

    updated to 17

--- Program.cs
+++ Program.cs
@@ -2,20 +2,10 @@
 class MyClass{
     public static void Main(string[] args){
 
-        int age = 10;
-        double height = 5.6;
-        string name = "Islam Hossain";
-        char grade = 'A'; //single quotation for character
-        bool isMarried = false; //bool stored true or false...
+        // 16. mathematical operator +, -, *, /, % operator precedence and associativity
+        // 17. Operators and their types - 
+        // Unary, Binary (arithmatic, asignment, relational, logical, bitwise), Ternary
 
-        Console.WriteLine("Age: " + age);
-        Console.WriteLine("Height: " + height);
-        Console.WriteLine("Name: " + name);
-        Console.WriteLine("Grade: " + grade);
-        Console.WriteLine("Married: " + isMarried);
-
-        string? inputName = Console.ReadLine();
-        Console.WriteLine("Hello " + inputName);
-
+        
     }
 }
